a2eef0575b6c3b5ad5016dfa4ce52ee5216c972a
Artem Bondar <dev@jet-mix.ru>
2021-02-05 17:44:02 +0300

    Some migrations deleted and changes made for checking DB connection

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    // Test database connection
+    try {
+        DB::connection()->getPdo();
+        echo "Connected successfully to: " . DB::connection()->getDatabaseName();
+    } catch (\Exception $e) {
+        die("Could not connect to the database. Please check your configuration. error:" . $e );
+    }
     return view('welcome');
 });
