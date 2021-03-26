b2beae37374cc559926ccfc478584fb82f47228a
Artem Bondar <dev@jet-mix.ru>
2021-03-26 18:54:51 +0300

    Fixed

--- app/Console/Commands/ParseComments.php
+++ app/Console/Commands/ParseComments.php
@@ -61,8 +61,9 @@ class ParseComments extends Command
 
         foreach ($articles as $article){
             $oldArticle = OldArticle::find($article->id);
-            if(!$oldArticle&&!is_null($oldArticle->thread_id)){
-                $oldComments = OldComment::where('thread_id','=',(int)$oldArticle->thread_id)->where('state','=',0)->cursor(); //OldArticle::find($article->id)->thread_id;
+//            if(is_null($oldArticle)){
+	     if(!is_null($oldArticle)){
+                $oldComments = OldComment::where('thread_id','=',$oldArticle->thread_id)->where('state','=',0)->cursor(); //OldArticle::find($article->id)->thread_id;
                 $oldThread = CommentThreads::where('id','=',$oldArticle->thread_id)->first();
                 foreach ($oldComments as $oldComment){
                     $comment = Comment::create(
@@ -75,15 +76,18 @@ class ParseComments extends Command
                             'children_count' => 0,
                             'user_id' => $oldComment->author_id,
                             'created_at' => $oldComment->created_at,
-                            'updated_at' => $oldComment->created_at
+                            'updated_at' => $oldComment->created_at,
+			    'answer_to' => null,
+			    'liked' => 0,
+			    'children_count' => 0,
                         ]
                     );
-                    $this->line($oldComment->id);
+		$this->line($oldComment->id);
             }
 
 
             }
-            if(!$oldArticle) {
+            if(!is_null($oldArticle)) {
                 $article->commented = $oldThread->num_comments;
                 $article->save;
             }
