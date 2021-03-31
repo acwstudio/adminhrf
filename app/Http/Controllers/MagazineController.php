cf8731bded4b4bfa3da0f810f28e93ce2f0df731
Evgeniy Makeev <devj@jet-mix.ru>
2021-03-31 05:23:57 +0200

    magazine

---
+++ app/Http/Controllers/MagazineController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\MagazineResource;
+use App\Models\Old\MagazineRelease;
+use Illuminate\Http\Request;
+
+class MagazineController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
+    public function index()
+    {
+        return MagazineResource::collection(MagazineRelease::with('categories', 'categories.article')->latest()->get());
+    }
+
+
+
+    public function show()
+    {
+        //
+    }
+
+
+
+}
