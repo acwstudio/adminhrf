4f6364e48f0ff45e5de4f3e042c54ffd4e990f46
Artem <artem_bondar98@mail.ru>
2021-04-05 17:35:42 +0200

    changed

--- app/Http/Resources/TimelineResource.php
+++ app/Http/Resources/TimelineResource.php
@@ -28,20 +28,9 @@ class TimelineResource extends JsonResource
             'views' => $this->timelinable->viewed,
             'has_like' => $user ? $this->timelinable->checkLiked($user) : false,
             'has_bookmark' => $user ? $this->timelinable->hasBookmark($user): false,
-
             'tags' => TagResource::collection($this->timelinable->tags),
             'comments' => $this->timelinable->commented,
             'image' => ImageResource::make($this->timelinable->images->first()),
-/*[
-                "model_type" => "image",
-                "id" => 1294,
-                "alt" => null,
-                "src" => "/images/articles/02/bwEmBMLhUWJBM5JT3VgHsDZ8NcVTWiytv99WSaxt.jpg",
-                "preview" => "/images/articles/02/bwEmBMLhUWJBM5JT3VgHsDZ8NcVTWiytv99WSaxt_min.jpg",
-                "original" => null,
-                "order" => 1
-            ],*/
-
         ];
     }
 }
