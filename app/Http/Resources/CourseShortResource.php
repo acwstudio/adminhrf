8ed684b53ad365f4ba836eebb67044b746e230ec
Evgeniy Makeev <devj@jet-mix.ru>
2021-04-06 20:36:05 +0200

    CourseResources fix

--- app/Http/Resources/CourseShortResource.php
+++ app/Http/Resources/CourseShortResource.php
@@ -27,15 +27,7 @@ class CourseShortResource extends JsonResource
             'views' => $this->viewed,
             'has_like' => $user ? $this->checkLiked($user) : false,
             'has_bookmark' => $user ? $this->hasBookmark($user): false,
-            'image' => [
-                "model_type" => "image",
-                "id" => 1294,
-                "alt" => null,
-                "src" => "/images/articles/02/bwEmBMLhUWJBM5JT3VgHsDZ8NcVTWiytv99WSaxt.jpg",
-                "preview" => "/images/articles/02/bwEmBMLhUWJBM5JT3VgHsDZ8NcVTWiytv99WSaxt_min.jpg",
-                "original" => null,
-                "order" => 1
-            ],
+            'image' => ImageResource::make($this->images()->first()),
         ];
     }
 }
