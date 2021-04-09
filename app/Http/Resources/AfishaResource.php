aa1829c66eece6b0f071f1434bf6350d7a562166
devjet-mix <dev@jet-mix.ru>
2021-04-09 09:35:57 +0000

    Fix for afisha resource

--- app/Http/Resources/AfishaResource.php
+++ app/Http/Resources/AfishaResource.php
@@ -25,12 +25,13 @@ class AfishaResource extends JsonResource
             'body' => $this->body,
             'street' => $this->street,
             'afisha_date' => $this->afisha_date,
-            'has_bookmark' => $user?$this->hasBookmark($user):false,
+#            'has_bookmark' => $user?$this->hasBookmark($user):false,
             'city' => CityResource::make($this->city),
             'leisure' => LeisureResource::make($this->leisure),
             'comments' => CommentResource::collection($this->comments),
             'likes' => $this->liked,
-            'image' => images->count()>0 ? ImageResource::make($this->images()->orderBy('order', 'asc')->first()) :[
+	    'has_like'=>$user?$this->checkLiked($user):false,
+            'image' => $this->images->count()>0 ? ImageResource::make($this->images()->orderBy('order', 'asc')->first()) :[
                 "model_type" => "image",
                 "id" => 1294,
                 "alt" => null,
