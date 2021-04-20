cccb3c4b3355f2fe4aaf3d46de39b8d8ddeb3de2
Artem <artem_bondar98@mail.ru>
2021-04-20 17:24:35 +0200

    Search resources added

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\ArticleSearchResource;
+use App\Http\Resources\BiographySearchResource;
+use App\Http\Resources\NewsSearchResource;
+use App\Http\Resources\TestSearchResource;
+use App\Http\Resources\VideomaterialSearchResource;
 use App\Models\Article;
 use App\Models\Biography;
 use App\Models\Highlight;
@@ -23,30 +27,30 @@ class SearchController extends Controller
     public function tests($query, Request $request)
     {
         $perPage = $request->get('per_page', $this->perPage);
-        return Test::search($query)->orderBy('published_at', 'desc')->paginate($perPage); //->union()
+        return TestSearchResource::collection(Test::search($query)->orderBy('published_at', 'desc')->paginate($perPage)); //->union()
     }
 
     public function biographies($query, Request $request)
     {
         $perPage = $request->get('per_page', $this->perPage);
-        return Biography::search($query)->orderBy('published_at', 'desc')->paginate($perPage); //->union()
+        return BiographySearchResource::collection(Biography::search($query)->orderBy('published_at', 'desc')->paginate($perPage));
     }
 
     public function news($query, Request $request)
     {
         $perPage = $request->get('per_page', $this->perPage);
-        return News::search($query)->orderBy('published_at', 'desc')->paginate($perPage); //->union()
+        return NewsSearchResource::collection(News::search($query)->orderBy('published_at', 'desc')->paginate($perPage));
     }
 
     public function highlights($query, Request $request)
     {
         $perPage = $request->get('per_page', $this->perPage);
-        return Highlight::search($query)->orderBy('published_at', 'desc')->paginate($perPage); //->union()
+        return Highlight::search($query)->orderBy('published_at', 'desc')->paginate($perPage);
     }
 
     public function videomaterials($query, Request $request)
     {
         $perPage = $request->get('per_page', $this->perPage);
-        return Videomaterial::search($query)->orderBy('published_at', 'desc')->paginate($perPage); //->union()
+        return VideomaterialSearchResource::collection(Videomaterial::search($query)->orderBy('published_at', 'desc')->paginate($perPage));
     }
 }
