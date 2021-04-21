c1ee77ae2eb6def23fa0db0fe80e42c80daf2d87
devjet-mix <dev@jet-mix.ru>
2021-04-21 12:00:27 +0000

    Biography route syntax error resolved

--- routes/api.php
+++ routes/api.php
@@ -173,7 +173,7 @@ Route::prefix('v1')->group(function () {
             Route::get('/search/{query}', [\App\Http\Controllers\SearchController::class, 'articles']);//old Route
             Route::get('/search/articles/{query}', [\App\Http\Controllers\SearchController::class, 'articles']);
             Route::get('/search/news/{query}', [\App\Http\Controllers\SearchController::class, 'news']);
-            Route::get('/search/biographies/{query}', [\App\Http\Controllers\SearchController::class, 'biograpies']);
+            Route::get('/search/biographies/{query}', [\App\Http\Controllers\SearchController::class, 'biographies']);
             Route::get('/search/tests/{query}', [\App\Http\Controllers\SearchController::class, 'tests']);
             Route::get('/search/videomaterials/{query}', [\App\Http\Controllers\SearchController::class, 'videomaterials']);
         }
