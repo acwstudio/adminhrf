a2d963be89cf91a27ecdb88bb1e2eda40bc7d8e2
Evgeniy Makeev <devj@jet-mix.ru>
2021-04-23 17:58:59 +0200

    User resource

--- app/Http/Resources/Admin/User/AdminUserResource.php
+++ app/Http/Resources/Admin/User/AdminUserResource.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources\Admin\User;
 
+use App\Http\Resources\Admin\AdminCommentResource;
+use App\Http\Resources\Admin\AdminImageResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 /**
@@ -24,21 +26,27 @@ class AdminUserResource extends JsonResource
             'attributes' => [
                 'name' => $this->name,
                 'email' => $this->email,
+                'email_verified_at' => $this->email_verified_at,
+                'legacy' => $this->legacy,
+                'created_at' => $this->created_at,
+                'updated_at' => $this->updated_at,
+                'status' => $this->status
             ],
             'relationships' => [
-                'role' => [
-                    'link' => [
-                        'self' => '',
-                        'related' => ''
-                    ],
+                'socials' => [
                     'data' => []
                 ],
-                'results' => [
-                    'link' => [
-                        'self' => '',
-                        'related' => ''
-                    ],
+                'role' => [
                     'data' => []
+                ],
+                'permissions' => [
+                    'data' => []
+                ],
+                'comments' => [
+                    'data' => AdminCommentResource::collection($this->whenLoaded('comments'))
+                ],
+                'image' => [
+                    'data' => AdminImageResource::make($this->whenLoaded('image'))
                 ]
             ]
         ];
